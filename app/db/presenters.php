26db9950e972a5aa199c13620336fe1bd9eacb0b
Pooya parsa <pi0@users.noreply.github.com>
2015-04-25 12:05:01 +0430

    Update presenters.php

--- app/db/presenters.php
+++ app/db/presenters.php
@@ -36,7 +36,7 @@ $presenters = [
     ],
 
     'nadalizadeh' => [
-        'name' => 'علی نادعلیزاده',
+        'name' => 'علی نادعلی‌زاده',
         'avatar' => '//www.gravatar.com/avatar/b048c6444f5618ca6ccd5dc33e125c10?s=200',
         'url' => 'http://ali.nadalizadeh.ir/',
         'bio' => 'مدیر فنی تاد'
@@ -46,28 +46,28 @@ $presenters = [
         'name' => 'فراز شمشیردار',
         'avatar' => '//www.gravatar.com/avatar/c4baac2a7eb2c03feb46429904f7098e?s=200',
         'url' => 'http://shamshirdar.net/',
-        'bio' => 'برنامه نویس در CNext'
+        'bio' => 'برنامه‌نویس در CNext'
     ],
 
     'mdhheydari' => [
         'name' => 'محمدحسین حیدری',
         'avatar' => '//www.gravatar.com/avatar/67a156a371ec4ffa39a37cfc4f824ee6?s=200',
         'url' => 'https://github.com/mdhheydari',
-        'bio' => 'توسعه‌دهنده نرم‌افزار'
+        'bio' => 'توسعه‌دهنده‌ی نرم‌افزار'
     ],
 
     'sinash' => [
         'name' => 'سینا شیخ الاسلامی',
         'avatar' => '//www.gravatar.com/avatar/9c773fbd22eab00f63f30a4b4a1f3a7a?s=200',
         'url' => 'http://sinash.ir/',
-        'bio' => 'توسعه دهنده‌ی نرم‌افزار'
+        'bio' => 'توسعه‌دهنده‌ی نرم‌افزار'
     ],
 
     'parham' => [
         'name' => 'پرهام الوانی',
         'avatar' => '//www.gravatar.com/avatar/1347add4ae303c0258a3db358ed77c55?s=200',
         'url' => 'http://1995parham.github.io/about/',
-        'bio' => 'توسعه دهنده‌ی نرم‌افزارهای متن‌باز'
+        'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
     ],
 
     'pooya' => [
@@ -81,7 +81,7 @@ $presenters = [
         'name' => 'زمین زمینی',
         'avatar' => '//www.gravatar.com/avatar/331f9ae8a4c8c6a33ba6ff3a1dcc3705?s=200',
         'url' => '',
-        'bio' => 'پروژه زمین'
+        'bio' => 'پروژه‌ی زمین'
     ],
 
 ];

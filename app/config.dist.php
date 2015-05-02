4758f1c22d3f347cc79f89145c4a4ee6a9c971a4
pi0 <pyapar@gmail.com>
2015-05-02 12:12:57 +0430

    Tracking code & SMS API

--- app/config.dist.php
+++ app/config.dist.php
@@ -8,6 +8,14 @@ define('GAPI_CLIENT_ID','');
 define('GAPI_CLIENT_SECRET','');
 define('GAPI_AUTH_REDIRECT_URL','http://linuxfest.ir/gauth/');
 
+define('SMS_URL','');
+define('SMS_from','');
+define('SMS_username','');
+define('SMS_password','');
+define('HASHID_SALT','');
+
+
+
 if(file_exists(GAPI_TOKEN_LOCATION))
     require_once(GAPI_TOKEN_LOCATION);
 

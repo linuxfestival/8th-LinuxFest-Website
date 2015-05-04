653bd0be6834ba59d621dc442322471221ae8818
Armin Bashizade <arminbashizade@yahoo.com>
2015-05-04 22:57:06 +0430

    Update presenters.php
    changed order

--- app/db/presenters.php
+++ app/db/presenters.php
@@ -4,14 +4,6 @@ global $presenters;
 
 $presenters = [
 
-    'peyberah' => [
-        'name' => 'امیرحسین پی‌براه',
-        'avatar' => '//www.gravatar.com/avatar/95062a379d77e40f6b73093d905be817?s=200',
-        'url' => 'https://www.sics.se/~amir/',
-        'bio' => 'پژوهشگر در SICS'
-    ],
-
-
     'bakhshis' => [
         'name' => 'بهادر بخشی',
         'avatar' => 'assets/img/bakhshi.jpg',
@@ -19,20 +11,11 @@ $presenters = [
         'bio' => 'استادیار دانشگاه صنعتی امیرکبیر'
     ],
 
-    'danial' => [
-        'name' => 'دانیال بهزادی',
-        'avatar' => '//www.gravatar.com/avatar/11db901e4c84a8017c6ed9aaaa2b70b8?s=200',
-        'url' => 'https://danialbehzadi.wordpress.com',
-        'bio' => 'گروه کاربران ایرانی اوبونتو'
-    ],
-
-
-
-    'mohajer' => [
-        'name' => 'میلاد مهاجر',
-        'avatar' => '//media.licdn.com/mpr/mpr/shrink_200_200/p/7/005/0a0/153/023ba11.jpg?s=200',
-        'url' => 'https://ir.linkedin.com/pub/milad-mohajer/a9/a19/444',
-        'bio' => 'مدیر بازرگانی رایمند'
+    'peyberah' => [
+        'name' => 'امیرحسین پی‌براه',
+        'avatar' => '//www.gravatar.com/avatar/95062a379d77e40f6b73093d905be817?s=200',
+        'url' => 'https://www.sics.se/~amir/',
+        'bio' => 'پژوهشگر در SICS'
     ],
 
     'nadalizadeh' => [
@@ -42,12 +25,32 @@ $presenters = [
         'bio' => 'مدیر فنی تاد'
     ],
 
+    'reza' => [
+        'name' => 'رضا محمدی',
+        'avatar' => '//www.gravatar.com/avatar/',
+        'url' => 'https://ir.linkedin.com/in/remohammadi',
+        'bio' => 'مدیر فنی کافه‌بازار'
+    ],
 
-    'salim' => [
-        'name' => 'سلیم ملکوتی',
-        'avatar' => '//www.gravatar.com/avatar/56c598844c7133a88576b3c81df3c309?s=200',
-        'url' => 'http://salimm.me/',
-        'bio' => 'دانشجوی دکترای دانشگاه پیتزبورگ'
+    'danial' => [
+        'name' => 'دانیال بهزادی',
+        'avatar' => '//www.gravatar.com/avatar/11db901e4c84a8017c6ed9aaaa2b70b8?s=200',
+        'url' => 'https://danialbehzadi.wordpress.com',
+        'bio' => 'گروه کاربران ایرانی اوبونتو'
+    ],
+
+    'peyman' => [
+        'name' => 'پیمان هوشمندی',
+        'avatar' => '//www.gravatar.com/avatar/31a61c55bc35852d5ad7d35ca129ce70?s=200',
+        'url' => 'http://arjang.ac.ir/instructor/2',
+        'bio' => 'موسسه ارژنگ'
+    ],
+
+    'mohajer' => [
+        'name' => 'میلاد مهاجر',
+        'avatar' => '//media.licdn.com/mpr/mpr/shrink_200_200/p/7/005/0a0/153/023ba11.jpg?s=200',
+        'url' => 'https://ir.linkedin.com/pub/milad-mohajer/a9/a19/444',
+        'bio' => 'مدیر بازرگانی رایمند'
     ],
 
     'faraz' => [
@@ -57,13 +60,6 @@ $presenters = [
         'bio' => 'برنامه‌نویس در CNext'
     ],
 
-    'mdhheydari' => [
-        'name' => 'محمدحسین حیدری',
-        'avatar' => '//www.gravatar.com/avatar/67a156a371ec4ffa39a37cfc4f824ee6?s=200',
-        'url' => 'https://ir.linkedin.com/in/mdhheydari',
-        'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
-    ],
-
     'sinash' => [
         'name' => 'سینا شیخ‌الاسلامی',
         'avatar' => '//www.gravatar.com/avatar/9c773fbd22eab00f63f30a4b4a1f3a7a?s=200',
@@ -71,6 +67,20 @@ $presenters = [
         'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
     ],
 
+    'mdhheydari' => [
+        'name' => 'محمدحسین حیدری',
+        'avatar' => '//www.gravatar.com/avatar/67a156a371ec4ffa39a37cfc4f824ee6?s=200',
+        'url' => 'https://ir.linkedin.com/in/mdhheydari',
+        'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
+    ],
+
+
+    'pooya' => [
+        'name' => 'پویا پارسا',
+        'avatar' => '//www.gravatar.com/avatar/1fddd58251edc7bf16e279b8811cd327?s=200',
+        'url' => 'http://pi0.ir/',
+        'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
+    ],
 
     'sinal' => [
         'name' => 'سینا لباف',
@@ -86,21 +96,5 @@ $presenters = [
         'url' => 'http://1995parham.github.io/about/',
         'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
     ],
-
-    'pooya' => [
-        'name' => 'پویا پارسا',
-        'avatar' => '//www.gravatar.com/avatar/1fddd58251edc7bf16e279b8811cd327?s=200',
-        'url' => 'http://pi0.ir/',
-        'bio' => 'توسعه‌دهنده‌ی نرم‌افزارهای متن‌باز'
-    ],
-
-
-    'peyman' => [
-        'name' => 'پیمان هوشمندی',
-        'avatar' => '//www.gravatar.com/avatar/31a61c55bc35852d5ad7d35ca129ce70?s=200',
-        'url' => 'http://arjang.ac.ir/instructor/2',
-        'bio' => 'موسسه ارژنگ'
-    ],
-
 
 ];

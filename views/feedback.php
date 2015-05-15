b8c674ca3e5f1fc91bfef1d5b11fe2a1fa75bf12
pi0 <pyapar@gmail.com>
2015-05-15 17:04:32 +0430

    update

---
+++ views/feedback.php
@@ -0,0 +1,13 @@
+<?php
+
+insertView('layout/header');
+
+?>
+
+
+<section id="" class="container">
+  <iframe src="https://docs.google.com/forms/d/1hDWpRK2cGoQmT8ielsFIsUUL3Jfekm73BodvIwqN-To/viewform?embedded=true" width="100%" height="1985" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+</section>
+
+<?php insertView('layout/footer'); ?>
+

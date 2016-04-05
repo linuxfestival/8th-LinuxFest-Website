37f86c8b30729b58975b432c9378c02d706797c8
pi0 <pyapar@gmail.com>
2016-04-05 13:11:07 +0430

    Sponsors and Contact US

--- resources/views/landing/sponsors.blade.php
+++ resources/views/landing/sponsors.blade.php
@@ -8,21 +8,12 @@
 
             <div class="row">
                 <div class="col-md-4">
-                    <a href="http://gotoclass.ir" target="_blank">
-                        <?php echo img('sponsors/gotoclass.png', 'class="sponsor"') ?>
-                    </a>
                 </div>
 
                 <div class="col-md-4">
-                    <a href="http://zoodfood.com" target="_blank">
-                        <?php echo img('sponsors/zoodfood.png', 'class="sponsor"') ?>
-                    </a>
                 </div>
 
                 <div class="col-md-4">
-                    <a href="http://cafebazaar.ir/jobs" target="_blank">
-                        <?php echo img('sponsors/bazaar.png', 'class="sponsor"') ?>
-                    </a>
                 </div>
 
 
@@ -47,24 +38,24 @@
 
                 <div class="col-md-2 col-md-offset-2">
                     <a href="http://ceit-ssc.ir/" target="_blank">
-                        <?php echo img('sponsors/aut_ssc.png', 'class="org"') ?>
+						<img src="{{asset('img/sponsors/aut_ssc.png')}}" class="org">
                     </a>
                 </div>
                 <div class="col-md-2">
                     <a href="http://sao.aut.ac.ir/" target="_blank">
-                        <?php echo img('sponsors/aut_federal.png', 'class="org"') ?>
+						<img src="{{asset('img/sponsors/aut_fedoral.png')}}" class="org">
                     </a>
                 </div>
 
                 <div class="col-md-2">
                     <a href="http://ce.aut.ac.ir/" target="_blank">
-                        <?php echo img('sponsors/aut_ceit.png', 'class="org"') ?>
+						<img src="{{asset('img/sponsors/aut_ceit.png')}}" class="org">
                     </a>
                 </div>
 
                 <div class="col-md-2">
                     <a href="http://aut.ac.ir/" target="_blank">
-                        <?php echo img('sponsors/aut.png', 'class="org"') ?>
+						<img src="{{asset('img/sponsors/aut.png')}}" class="org">
                     </a>
                 </div>
 

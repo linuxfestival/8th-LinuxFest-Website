92b3dd32cf67b20b8661837e5e67195ab6dbfab9
Pooya Parsa <pi0@users.noreply.github.com>
2016-04-08 18:38:49 +0430

    Meta

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="هفتمین جشنواره‌ی لینوکس دانشگاه صنعتی امیرکبیر">
+    <meta name="description" content="هشتمین جشنواره‌ی لینوکس دانشگاه صنعتی امیرکبیر"  >
 
     <title>
         جشنواره‌ی لینوکس امیرکبیر

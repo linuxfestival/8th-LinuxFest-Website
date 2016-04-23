2d2e60d7965ae605d4955c4b29fc479e584a7925
Haghighati <9331009@ceit.local>
2016-04-23 13:04:17 +0430

    Press Sponsers added

--- resources/views/landing/sponsors.blade.php
+++ resources/views/landing/sponsors.blade.php
@@ -1,14 +1,17 @@
-<!--
+
 <aside class="bg-yellow" id="sponsors">
     <div class="container text-center">
         <div class="call-to-action">
 
             <h2>
-                حامیان جشنواره
+حامیان رسانه‌ای
             </h2>
 
             <div class="row">
                 <div class="col-md-4">
+                   <a href="http://peidad.com/" target="_blank">
+                        <img src="{{asset('img/sponsors/peidad.png')}}" class="org">
+                   </a>
                 </div>
 
                 <div class="col-md-4">
@@ -25,7 +28,6 @@
         </div>
     </div>
 </aside>
--->
 
 <aside>
     <div class="container text-center">

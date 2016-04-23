1a3e6ce119fc09262581ef5ea28796b04af6e01d
anewage <amirh2008@gmail.com>
2016-04-23 13:15:10 +0430

    Press Sponsers added

--- resources/views/landing/sponsors.blade.php
+++ resources/views/landing/sponsors.blade.php
@@ -1,5 +1,5 @@
 
-<aside class="bg-yellow" id="sponsors">
+<aside class="bg-yellow" id="press-sponsors">
     <div class="container text-center">
         <div class="call-to-action">
 
@@ -29,6 +29,37 @@
         </div>
     </div>
 </aside>
+
+<!--<aside class="bg-boo" id="sponsors">-->
+<!--    <div class="container text-center">-->
+<!--        <div class="call-to-action">-->
+<!---->
+<!--            <h2>-->
+<!--حامیان رویداد-->
+<!--            </h2>-->
+<!---->
+<!--            <div class="row">-->
+<!--                <div class="col-md-4">-->
+<!---->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-md-4">-->
+<!--                    <a href="http://peidad.com/" target="_blank">-->
+<!--                        <img src="{{asset('img/sponsors/peidad.png')}}" class="org">-->
+<!--                    </a>-->
+<!--                </div>-->
+<!---->
+<!--                <div class="col-md-4">-->
+<!--                </div>-->
+<!---->
+<!---->
+<!---->
+<!--            </div>-->
+<!---->
+<!---->
+<!--        </div>-->
+<!--    </div>-->
+<!--</aside>-->
 
 <aside>
     <div class="container text-center">

abbb05d33e44629d38723986d6b8651ace955329
pi0 <pyapar@gmail.com>
2016-04-29 17:51:13 +0430

    Unescape Description

--- resources/views/section/section.blade.php
+++ resources/views/section/section.blade.php
@@ -50,7 +50,7 @@
                 </div>
                 <div class="col-md-8">
                     <p>
-                        {{$section->desc}}
+                        {!! $section->desc !!}
                     </p>
                 </div>
             </div>

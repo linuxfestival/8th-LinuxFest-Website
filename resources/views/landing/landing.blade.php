a52ef8a469c07cfd703d5f09685a0d607a2ea922
anewage <amirh2008@gmail.com>
2016-05-03 16:56:19 +0430

    sections updated

--- resources/views/landing/landing.blade.php
+++ resources/views/landing/landing.blade.php
@@ -3,8 +3,8 @@
 @section('content')
     @include('landing.includes.header')
     @include('landing.includes.about')
-    @include('landing.includes.special')
-{{--    @include('landing.includes.sections')--}}
+{{--    @include('landing.includes.special')--}}
+    @include('landing.includes.sections')
     @include('landing.includes.presenters')
     @include('landing.includes.sponsors')
     @include('landing.includes.contact')

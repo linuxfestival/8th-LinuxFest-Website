36c2f6ba0cdda55709d067bcf8f903dc60a3b5a6
Amir Haghighati <amirh2008@gmail.com>
2017-03-12 00:28:27 +0330

    Resolves #28

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -18,18 +18,18 @@ class Authenticate
      */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->guest()) {
-            if ($request->ajax() || $request->wantsJson()) {
-                return response('Unauthorized.', 401);
-            } else {
-                return redirect()->guest('login');
-            }
-        }
         if (Auth::getUser() != null){
             /** @var User $user */
             $user = Auth::user();
             if ($user->type != User::$ADMIN){
                 Auth::logout();
+                return redirect()->guest('login');
+            }
+        }
+        if (Auth::guard($guard)->guest()) {
+            if ($request->ajax() || $request->wantsJson()) {
+                return response('Unauthorized.', 401);
+            } else {
                 return redirect()->guest('login');
             }
         }

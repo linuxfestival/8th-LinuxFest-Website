7c12c5de25549d7847ea434a53c9ec6d6085fc22
Amir Haghighati <amirh2008@gmail.com>
2017-03-13 13:15:34 +0330

    error blades updated.

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
         <section class="session-container">
             <div style="text-align: center">
                 <h1>
-                    ۵۰۳ - به زودی بازمیگردیم!
+                    ۴۰۴ - چیزی پیدا نشد!
                 </h1>
             </div>
             <hr>

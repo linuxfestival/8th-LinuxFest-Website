b76d38d21e71e415932c74278158f52bac2e934e
kimiarm <kimia.rezaei96@gmail.com>
2017-03-26 03:38:52 -0700

    AdminController Indentation corrected

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -31,22 +31,18 @@ class AdminController extends Controller
             Route::post('/submissions/sponsors/{sponsor}', 'AdminController@updateSponsorRequest')->name('admin::submissions.sponsor.save');
             Route::get('/submissions/{submission}', 'AdminController@editSubmission')->name('admin::submissions.edit');
             Route::post('/submissions/{submission}', 'AdminController@updateSubmission')->name('admin::submission.save') ;
-                 //Presenter
+            //Presenter
             Route::get('/presenters', 'AdminController@displayPresenter')->name('admin::presenters');
             Route::get('/presenters/add', 'AdminController@addPresenter')->name('admin::presenter.add');
             Route::post('/presenters/add', 'AdminController@storeAddingPresenterRequest')->name('admin::presenter.submit');
             Route::get('/presenters/{presenter}', 'AdminController@editPresenter')->name('admin::presenter.edit');
             Route::post('/presenters/{presenter}', 'AdminController@updatePresenter')->name('admin::presenter.save');
-
             //Users section
             Route::get('/users', 'AdminController@listUsers')->name('admin::users');
-
             // Live Blog Admin Panel
             Route::get('/live','AdminController@liveIndex')->name('admin::live.index');
-
             //POST on live
             Route::post('message', ['as' => 'message.store', 'uses' => 'AdminController@store']);
-
             //Sending mail
             Route::post('/email', 'AdminController@viewMailForm')->name('admin::mail.compose');
             Route::post('/email/submit', 'AdminController@sendMail')->name('admin::mail.send');
@@ -62,47 +58,59 @@ class AdminController extends Controller
         //TODO ADMIN DASHBOARD
         return view('admin.home');
     }
+
     /**
-     * display presenters
+     * display all presenters in admin Dashboard
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function displayPresenter(){
         $presenters = Presenter::all();
-
         return view('admin.Presenters.display', ['presenters' => $presenters]) ;
     }
-public function editPresenter(Presenter $presenter){
-    return view('admin.Presenters.edit', ['data' => $presenter]);
-}
+
+    /**
+     * @param Presenter $presenter
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function editPresenter(Presenter $presenter){
+        return view('admin.Presenters.edit', ['data' => $presenter]);
+    }
+
+    /**
+     * @param Presenter $presenter
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function updatePresenter(Presenter $presenter, Request $request){
-   // dd($request->all());if ($request->hasFile('presenter_avatar'))
         $presenter->update($request->all());
         $presenter->save();
-if($request->hasFile('presenter_avatar'))
-        if ($request->file('presenter_avatar')->isValid()){
-            $directory = 'storage/presenter/' . $presenter->_id . '/';
-            $file_name = 'presenter_avatar' ;
-            $presenter->presenter_avatar = $file_name;
-
-            $presenter->save();
-            $request->file('presenter_avatar')->move($directory, $file_name);
-
-
-        }
-
-
+        if($request->hasFile('presenter_avatar'))
+            if ($request->file('presenter_avatar')->isValid()){
+                $directory = 'storage/presenter/' . $presenter->_id . '/';
+                $file_name = 'presenter_avatar' ;
+                $presenter->presenter_avatar = $file_name;
+                $presenter->save();
+                $request->file('presenter_avatar')->move($directory, $file_name);
+            }
         return redirect()->route('admin::presenters');
     }
+
     /**
      * Add new presenter
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function addPresenter(){
         return view('admin.Presenters.add');
     }
-    public function storeAddingPresenterRequest(Request $request){
 
-     $g_response = $this->validateRECAPTCHA($request);
-         if (!$g_response["status"])
-       return redirect()->back()->withErrors($g_response["res"]);
+    /**
+     * @param Request $request
+     * @return $this|\Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function storeAddingPresenterRequest(Request $request){
+        $g_response = $this->validateRECAPTCHA($request);
+        if (!$g_response["status"])
+            return redirect()->back()->withErrors($g_response["res"]);
         $validator = \Validator::make($request->all(), \App\Http\Requests\Presentering::getRules());
         if ($validator->fails()){
             dd($validator->errors()->all());
@@ -112,17 +120,13 @@ if($request->hasFile('presenter_avatar'))
         // Storing the data
         $s = new Presenter($request->all());
         $s->save();
-
         if ($request->hasFile('presenter_avatar'))
             if ($request->file('presenter_avatar')->isValid()){
                 $directory = 'storage/presenter/' . $s->_id . '/';
                 $file_name = 'presenter_avatar' ;
                 $s->presenter_avatar = $file_name;
-
                 $s->save();
                 $request->file('presenter_avatar')->move($directory, $file_name);
-
-
             }
         return view('admin.Presenters.success', ['data' => $s]);
     }
@@ -176,6 +180,11 @@ if($request->hasFile('presenter_avatar'))
 //            $_to = [$_to];
         return view('admin.outgoing.compose', ['to' => $_to]);
     }
+
+    /**
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
 
     public function sendMail(Request $request){
         $to = $request->get('to');
@@ -357,6 +366,10 @@ if($request->hasFile('presenter_avatar'))
 //
 //        return Redirect::route('admin');
 //    }
+    /**
+     * @param Request $request
+     * @return array
+     */
     private function validateRECAPTCHA(Request $request) {
         if (!$request->has('g-recaptcha-response'))
             return ["status" => false, "res" => "Empty response"];

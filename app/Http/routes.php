f2e8a662252ee37eb73f491a859370c8e41dcd8b
Parham Alvani <parham.alvani@gmail.com>
2017-04-09 06:40:22 +0430

    Route file was reformatted.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -9,50 +9,58 @@
 | It's a breeze. Simply tell Laravel the URIs it should respond to
 | and give it the controller to call when that URI is requested.
 |
-*/
+ */
 use App\models2016\Presenter;
 use App\models2016\Section;
 use App\models2016\Sponsor;
 use Illuminate\Support\Facades\Route;
 
 Route::group(
-    ['middleware' => ['web']], function () {
+    ['middleware' => ['web']],
+    function () {
 
-    // Landing
-    \App\Http\Controllers\SiteController::routes();
+        // Landing
+        \App\Http\Controllers\SiteController::routes();
 
-    //Admin
-    \App\Http\Controllers\AdminController::routes();
+        //Admin
+        \App\Http\Controllers\AdminController::routes();
 
-});
+    }
+);
 Route::auth();
 
 Route::get('/home', 'HomeController@index');
-Route::get('/old_2016',function (){
-    $presenters = Presenter::all();
-    $workshops = Section::whereType('workshop')->get();
-    $presentations = Section::whereType('presentation')->get();
-    $sponsors = Sponsor::all();
+Route::get(
+    '/old_2016',
+    function () {
+        $presenters = Presenter::all();
+        $workshops = Section::whereType('workshop')->get();
+        $presentations = Section::whereType('presentation')->get();
+        $sponsors = Sponsor::all();
 
-    return view('2016.landing.landing', [
-        'presenters' => $presenters,
-        'sections' => [
-            'کارگاه ها'=>$workshops,
-            'ارائه ها'=>$presentations,
-        ],
-        'sponsors' => $sponsors,
-    ]);
-});
+        return view('2016.landing.landing', [
+            'presenters' => $presenters,
+            'sections' => [
+                'کارگاه ها'=>$workshops,
+                'ارائه ها'=>$presentations,
+            ],
+            'sponsors' => $sponsors,
+        ]);
+    }
+);
 
-Route::get('/2016/{section}', function ($section){
+Route::get('/2016/{section}', function ($section) {
     $presenters = [];
 
-    if($section->presenter)
-        $presenters[]=Presenter::where('id',$section->presenter)->firstOrFail();
+    if ($section->presenter) {
+        $presenters[]=Presentesr::where('id', $section->presenter)->firstOrFail();
+    }
 
-    if($section->presenters)
-        foreach ($section->presenters as $presenter)
-            $presenters[]=Presenter::where('id',$presenter)->firstOrFail();
+    if ($section->presenters) {
+        foreach ($section->presenters as $presenter) {
+            $presenters[]=Presenter::where('id', $presenter)->firstOrFail();
+        }
+    }
 
     return view('section.section', [
         'section' => $section,
